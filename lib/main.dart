bd8bd99e046e209cd55ad710bb547858d32aee68
Fabio Lafayete <fabio.goncalves@valemobi.com.br>
2022-07-22 19:38:36 -0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(
+      GetMaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+          body: Center(child: Text('inicio'),),
+        ),
+      )
+  );
+}
