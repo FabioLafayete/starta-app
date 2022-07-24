ec05dca1e52a2c27c5d3e107084dcf07da866f34
Fabio Lafayete <f.lafayete@gmail.com>
2022-07-24 01:07:25 -0300

    init movie details

---
+++ lib/modules/movie_detail/view_model/movie_details_view_model.dart
@@ -0,0 +1,11 @@
+import 'package:get/get.dart';
+
+class MovieDetailsViewModel extends GetxController with StateMixin{
+
+  MovieDetailsViewModel() : super(){
+    getData();
+  }
+
+  getData(){}
+
+}
